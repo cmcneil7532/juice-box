1e036daaf511d6a5b6bbdc15f0bb1fd4c9292c24
Christian McNeil <christianmcneil@christians-mbp.mynetworksettings.com>
2023-04-21 15:08:55 -0400

    Implement CRUD on tags and posts

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,34 @@
-const { client, getAllUsers, createUser, updateUser } = require("./index");
+const {
+  client,
+  getAllUsers,
+  createUser,
+  updateUser,
+  createPost,
+  getAllPosts,
+  getPostsByUser,
+  getUserById,
+  createTags,
+  getPostById,
+  addTagsToPost,
+  updatePost,
+  getPostsByTagName,
+} = require("./index");
 
 async function dropTables() {
   try {
     console.log("Begin dropping table!");
+
     await client.query(`
-        DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS post_tags;
+    `);
+    await client.query(`
+    DROP TABLE IF EXISTS tags;
+    `);
+    await client.query(`
+    DROP TABLE IF EXISTS posts;
+    `);
+    await client.query(`
+    DROP TABLE IF EXISTS users;
         `);
     console.log("Finished dropping table!");
   } catch (error) {
@@ -16,16 +40,41 @@ async function dropTables() {
 async function createTables() {
   try {
     console.log("Creating table!");
+
     await client.query(`
     CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      name VARCHAR(255) NOT NULL,
+      location VARCHAR(255) NOT NULL,
+      active BOOLEAN DEFAULT true
+      );
+      `);
+
+    await client.query(`
+      CREATE TABLE posts(
         id SERIAL PRIMARY KEY,
-        username VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        name VARCHAR(255) NOT NULL,
-        location VARCHAR(255) NOT NULL,
+        "authorId" INTEGER REFERENCES users(id) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        content TEXT NOT NULL,
         active BOOLEAN DEFAULT true
-    );
+        );
+        `);
+    await client.query(`
+      CREATE TABLE tags(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL
+            );
+            `);
+    await client.query(`
+      CREATE TABLE post_tags(
+        "postId" INTEGER REFERENCES posts(id),
+        "tagId" INTEGER REFERENCES tags(id),
+        UNIQUE("postId", "tagId")
+        );
     `);
+
     console.log("Finished creating table!");
   } catch (error) {
     console.error(error);
@@ -68,12 +117,40 @@ const createInitialUsers = async () => {
   }
 };
 
+const createInitialPosts = async () => {
+  try {
+    const [sandra, albert, glamgal, christian] = await getAllUsers();
+
+    await createPost({
+      authorId: sandra.id, //Refernce our author id with users id and push those posts to the user
+      title: "Sandra Post",
+      content: "this is my first post",
+      tags: ["#happy", "#youcandoanything"],
+    });
+    await createPost({
+      authorId: christian.id,
+      title: "Christian Post",
+      content: "this is my first post",
+      tags: ["#happy", "#worst-day-ever"],
+    });
+    await createPost({
+      authorId: albert.id,
+      title: "Albert Post",
+      content: "this is my first post",
+      tags: ["#happy", "#youcandoanything", "#canmandoeverything"],
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 async function rebuildDB() {
   try {
     client.connect();
     await dropTables();
     await createTables();
     await createInitialUsers();
+    await createInitialPosts();
   } catch (error) {
     console.error(error);
   }
@@ -82,16 +159,17 @@ async function rebuildDB() {
 async function testDB() {
   try {
     console.log("Starting to test database...");
-    const users = await getAllUsers();
-    console.log("getAllUsers: ", users);
+    // const users = await getAllUsers();
+    // console.log("getAllUsers: ", users);
+    // console.log("Creating tags");
+    // const createdTags = await createTags(["#tag", "#othertag", "#onemoretag"]);
+    // console.log(createdTags);
+    // console.log("Finished Creating tags");
 
-    console.log("Calling updateUser on users[0]");
+    console.log("Calling getPostsByTagName with #happy");
+    const postsWithHappy = await getPostsByTagName("#happy");
+    console.log("Result:", postsWithHappy);
 
-    const updatedUser = await updateUser(users[0].id, {
-      name: "Newname Sogood",
-      location: "Lesterville, KY",
-    });
-    console.log(updatedUser);
     console.log("Finished database tests!");
   } catch (error) {
     console.error(error);
