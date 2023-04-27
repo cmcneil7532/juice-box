2cf726f24d11c0e3b62ec810eb31b5599b032a4d
Christian McNeil <christianmcneil@christians-mbp.mynetworksettings.com>
2023-04-27 18:12:51 -0400

    Incorporate cors for the client

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const { client } = require("./db");
-
+const cors = require("cors");
 client.connect(); //connect our database to our server
+app.use(cors());
 app.use(morgan("dev")); //this function logs out the incoming requests
 app.use(express.json()); //function which reads incoming JSON from requests
 
