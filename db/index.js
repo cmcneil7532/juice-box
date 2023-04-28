3a5845a106d0bd70bae05e95418c6427187153db
Christian McNeil <christianmcneil@christians-mbp.mynetworksettings.com>
2023-04-28 10:59:05 -0400

    refactor

--- db/index.js
+++ db/index.js
@@ -1,14 +1,17 @@
 const { Client } = require("pg"); // import pg module
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const DATABASE_URL = process.env;
+const { DATABASE_URL } = process.env;
 //Supply the database name and location of the database
 const connectionString =
   DATABASE_URL || "postgres://localhost:5432/juicebox-dev";
 
 const client = new Client({
   connectionString,
-  ssl: { rejectUnauthorized: false },
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 //Grab all
